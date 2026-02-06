ff5a4dcfdff2496b4427feb585fff0605e8d7ff8
Gabriel-Matias <gabrielmf1977@gmail.com>
2026-02-06 12:57:06 +0000

    Fixed bit set

--- include/bitset.h
+++ include/bitset.h
@@ -5,34 +5,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifndef BITSET_SIZE
+#define BITSET_SIZE 128
+#endif
+
 typedef struct BitSet
 {
-    char *array;
-    int size;
+    char array[BITSET_SIZE / 8];
 } BitSet;
 
-#define bit_set_init(_size)                                     \
-    (BitSet)                                                    \
-    {                                                           \
-        .array = malloc((_size + sizeof(char) / sizeof(char))), \
-        .size = _size,                                          \
+#define bit_set_init  \
+    (BitSet)          \
+    {                 \
+        .array = {0}, \
     }
 
 #define ___bit_set_check_bounds(bit_set, n)             \
     {                                                   \
-        if (n > bit_set.size)                           \
+        if (n > BITSET_SIZE)                            \
         {                                               \
             printf("Cannot set bit outside of size\n"); \
             exit(1);                                    \
         }                                               \
     }
 
-#define ___bit_set_get_word(bit_set, n) (bit_set.array[n / sizeof(char)])
+#define ___bit_set_get_word(bit_set, n) (bit_set.array[n / 8])
 
 #define ___bit_set_set_word(bit_set, n, word)   \
     do                                          \
     {                                           \
-        bit_set.array[n / sizeof(char)] = word; \
+        bit_set.array[n / 8] = word; \
     } while (0)
 
 #define bit_set_set(bit_set, n)                                                              \
--- test/bitset_tests.c
+++ test/bitset_tests.c
@@ -1,40 +1,68 @@
+#define BITSET_SIZE 128
 #include "bitset.h"
 #include <assert.h>
 #include <stdio.h>
 
 int main()
 {
-    BitSet set = bit_set_init(99);
-    assert(set.size == 99);
+    BitSet set = bit_set_init;
+    for (int i = 0; i < 128; i++)
+    {
+        printf("\\ %d:%d\n",i, i / 8);
+        printf("%% %d:%d\n",i, i % 8);
+    }
     for (int i = 0; i < 99; i++)
     {
+        printf("I:%d: %u\n", i, bit_set_get(set, i) ? 1 : 0);
+        assert(!bit_set_get(set, i));
+    }
+    for (int i = 0; i < 99; i++)
+    {
+        printf("Setting %i\n", i);
         bit_set_set(set, i);
     }
     for (int i = 0; i < 99; i++)
     {
+        printf("I:%d: %u\n", i, bit_set_get(set, i) ? 1 : 0);
         assert(bit_set_get(set, i));
     }
     for (int i = 0; i < 99; i++)
     {
         if (i % 2 == 0)
+        {
+            printf("Clearing %i\n", i);
             bit_set_clear(set, i);
+        }
     }
     for (int i = 0; i < 99; i++)
     {
         if (i % 2 != 0)
+        {
+            printf("I:%d: %u\n", i, bit_set_get(set, i) ? 1 : 0);
             assert(bit_set_get(set, i));
+        }
         if (i % 2 == 0)
+        {
+            printf("I:%d: %u\n", i, bit_set_get(set, i) ? 1 : 0);
             assert(!bit_set_get(set, i));
+        }
     }
     for (int i = 0; i < 99; i++)
     {
+        printf("Toggling %i\n", i);
         bit_set_toggle(set, i);
     }
     for (int i = 0; i < 99; i++)
     {
         if (i % 2 == 0)
+        {
+            printf("I:%d: %u\n", i, bit_set_get(set, i) ? 1 : 0);
             assert(bit_set_get(set, i));
+        }
         if (i % 2 != 0)
+        {
+            printf("I:%d: %u\n", i, bit_set_get(set, i) ? 1 : 0);
             assert(!bit_set_get(set, i));
+        }
     }
 }
